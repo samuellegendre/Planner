0b3c977ed793a2dc3c7336effbde24c337459501
samuellegendre <samuel.legendre@outlook.com>
2021-04-12 15:44:12 -0400

    Ajout des interfaces pour les tâches

--- app/src/main/java/com/example/planner/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/planner/ui/tasks/TasksFragment.kt
@@ -3,50 +3,29 @@ package com.example.planner.ui.tasks
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
-import android.widget.PopupMenu
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.example.planner.NotificationsActivity
 import com.example.planner.R
 import com.example.planner.SearchableActivity
-import com.example.planner.ui.calendar.AddClassDialogFragment
-import com.example.planner.ui.calendar.Event
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class TasksFragment : Fragment(), AddTaskDialogFragment.AddTaskDialogListener, AddListDialogFragment.AddListDialogListener {
+class TasksFragment : Fragment(), AddTaskDialogFragment.AddTaskDialogListener,
+    AddListDialogFragment.AddListDialogListener {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
         val view: View = inflater.inflate(R.layout.fragment_tasks, container, false)
-        val addTaskButton: Button = view.findViewById(R.id.addTaskButton)
-        val addListButton: Button = view.findViewById(R.id.addListButton)
-        val listActions: Button = view.findViewById(R.id.listActions)
-        val taskActions: Button = view.findViewById(R.id.taskActions)
+        val addTaskButton: FloatingActionButton = view.findViewById(R.id.addTaskButton)
 
         addTaskButton.setOnClickListener {
             val dialog = AddTaskDialogFragment()
             dialog.show(childFragmentManager, "addTask")
-        }
-        addListButton.setOnClickListener {
-            val dialog = AddListDialogFragment()
-            dialog.show(childFragmentManager, "addList")
-        }
-        listActions.setOnClickListener {
-            val popup = PopupMenu(requireContext(), listActions)
-            val inflater: MenuInflater = popup.menuInflater
-            inflater.inflate(R.menu.fragment_tasks_list_dropdown, popup.menu)
-            popup.show()
-        }
-        taskActions.setOnClickListener {
-            val popup = PopupMenu(requireContext(), taskActions)
-            val inflater: MenuInflater = popup.menuInflater
-            inflater.inflate(R.menu.fragment_tasks_task_dropdown, popup.menu)
-            popup.show()
         }
 
         return view
